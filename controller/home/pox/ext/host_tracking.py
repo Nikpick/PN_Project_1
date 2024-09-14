7ff9700f2a1ea114c139e9bd619e0fc76ac996ab
Nikpick <gly.nkt24@gmail.com>
2024-09-14 11:15:08 +0200

    Added test results.

--- controller/home/pox/ext/host_tracking.py
+++ controller/home/pox/ext/host_tracking.py
@@ -32,12 +32,9 @@ class HostTracking(EventMixin):
 
             #first I find all the links where the current switch is in the first position
             possible_links = [link for link in links if link.sid1 == current_switch.sid]
-            #for link in possible_links:
-            #    print(link.name)
 
             #excluding s2 (since it only handles GW packets), if the interface the packet is coming from is not part of the possible links, then is a new connection from a mobile
             not_a_link = current_switch.name != "s2" and all((link.port1 != current_interface) for link in possible_links)
-            #print(not_a_link)
             current_connection = (current_switch.name, current_interface)
             if  not_a_link:
                 if (current_connection != self.last_connection):
